8f1992ad2637ba58f8a26a94f3045448ea58b288
“Satyam <satyam@gmail.com>
2024-05-13 08:35:34 +0530

    hello

---
+++ src/main/java/easymytrip/example/easymytrip/hotel.java
@@ -0,0 +1,11 @@
+package easymytrip.example.easymytrip;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hotel {
+    @GetMapping("/hotel")
+    public String getData() {return  "Please book your hotel ticket for shimla t 25% discount" ; }
+}
