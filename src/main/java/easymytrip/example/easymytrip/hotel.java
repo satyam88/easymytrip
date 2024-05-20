7efd91973d3bca769e2f8daf542bf43e6a0ab6fa
satyam88 <satyampandey88@gmail.com>
2024-05-20 07:19:04 +0530

    Update hotel.java

--- src/main/java/easymytrip/example/easymytrip/hotel.java
+++ src/main/java/easymytrip/example/easymytrip/hotel.java
@@ -7,5 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class hotel {
     @GetMapping("/hotel")
-    public String getData() {return  "Please book your hotel ticket for shimla t 25% discount" ; }
+    public String getData() {return  "Please book your hotel ticket for shimla t 35% discount" ; }
 }
